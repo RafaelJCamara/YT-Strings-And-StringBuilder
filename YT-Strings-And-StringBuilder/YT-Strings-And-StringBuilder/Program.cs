ccc0cdb0f19723d923902e026efcfb834f2a7cbe
RafaelJCamara <rafaelcamarac@gmail.com>
2025-09-16 21:07:30 +0200

    Add string and StringBuilder source code.

---
+++ YT-Strings-And-StringBuilder/YT-Strings-And-StringBuilder/Program.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+//var str = "";
+
+//for (var i = 0; i < 100; i++)
+//{
+//    //append to string in each iteration
+//    str += i;
+//}
+
+//var sb = new StringBuilder("Hello start ");
+//sb.Append("Hello, ");
+
+//Console.WriteLine(sb.ToString());
+
+[MemoryDiagnoser]
+public class StringVsStringBuilderBenchmarks
+{
+    [Params(1,10,100,1_000,10_000)]
+    public int NumOfConcats;
+
+    [Benchmark(Baseline = true)]
+    public string PlusConcatenation()
+    {
+        var str = "";
+        for (var i = 0; i < NumOfConcats; i++)
+        {
+            str += i;
+        }
+        return str;
+    }
+
+    [Benchmark]
+    public string StringBuilderConcatenation()
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < NumOfConcats; i++)
+        {
+            sb.Append(i);
+        }
+        return sb.ToString();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<StringVsStringBuilderBenchmarks>();
+    }
+}
